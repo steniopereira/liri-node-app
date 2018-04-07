485083b01857891e145f220edbab227c1c999db0
Stenio Pereira <steniocp@gmail.com>
2018-04-07 09:40:01 -0700

    final rev

--- liri.js
+++ liri.js
@@ -14,31 +14,28 @@ var keys = require("./keys");
   	access_token_secret: keys.twitterKeys.access_token_secret
 	});
 
-//my account "@steliopartidus"
+// my account "@steliopartidus"
 	var myTweets = {screen_name:"steliopartidus"};
 
 // Spotify Code
-	//to include the Node Spotify API npm package
+	// to include the Node Spotify API npm package
 	var Spotify = require("node-spotify-api");
 
-//pulling Node Spotify API info from keys.js
+// pulling Node Spotify API info from keys.js
 	var spotify = new Spotify ({
 	id: keys.spotifyKeys.id,
 	secret: keys.spotifyKeys.secret
 	});
 
-//variable to hold song the user wants to search for with Spotify
+// variable to hold song the user wants to search for with Spotify
 	var songChoice = "";
 
 // OMDB Code 
-//to include the Request API npm package
-//this will be used for the Open Movie Database (OMDB)
+// to include the Request API npm package
+// this will be used for the Open Movie Database (OMDB)
 var request = require("request");
 
-//the key to use OMDB
-//var omdbKey = "40e9cece"; //Just for reference. I ended up not using this variable
-
-//variable to hold movie the user wants to search for with OMDB
+// variable to hold movie the user wants to search for with OMDB
 var omdbRequest = "";
 
 //to include the fs library
@@ -71,13 +68,11 @@ else if (userCommand === "do-what-it-says") {
 
 //if the user forgets to put in a command for process.argv[2]
 //or if the command is spelled incorrectly
-else {
-		return console.log("There was an error.");
+else {return console.log("There was an error.");
 	}
 
 //======================= THE FUNCTIONS =======================
-
-//function for the twitter calls
+//function for the Twitter calls
 function twitterCall() {
 	twitter.get("statuses/user_timeline", myTweets, function(error, tweets, response){
 		if(error) {
@@ -94,8 +89,6 @@ function twitterCall() {
 				//convert UTC time to local time for tweet timestamp part 2
 				console.log(tweetDate.toString()); 
 				console.log("------------------------------------------");
-
-				//console.log(JSON.stringify(tweets, null, 2)); //<< prints out entire response of call. saving for reference.
 			}
 		}
 	});
@@ -103,21 +96,19 @@ function twitterCall() {
 
 //functions for the spotify calls
 //spotifyCheck() figures out what the user is searching and puts it in variable songChoice
-//spotifyCall() executes the search with songChoice
-
 function spotifyCheck () {
-	//if the user doesn't put in a song, the default will be Blitzkrieg Bop by Lady Gaga
+	//if the user doesn't put in a song, the default will be Blitzkrieg Bop by The Ramones
 	if (!process.argv[3]) {
 		songChoice = "Blitzkrieg Bop";
 	} else {
 		//this accomodates song titles with multiple words in it
 		for (j = 3; j < process.argv.length; j++) {
 			songChoice = process.argv[j];
-			// console.log(songChoice); //test
-		}
+				}
 	}
 };
 
+//spotifyCall() executes the search with songChoice
 function spotifyCall(songChoice) {
 	spotify.search({type: "track", query: songChoice, limit: 1}, function(error, response){
 		if (error) {
@@ -149,14 +140,13 @@ function movieCheck() {
 
 function movieCall(omdbRequest) {
 	//variable to hold the omdb url search with api key and omdb request
-	var omdbMovie = "http://www.omdbapi.com/?apikey=40e9cece&t=" + omdbRequest;
+	var omdbMovie = "http://www.omdbapi.com/?apikey=27b506d4&t=" + omdbRequest;
 
-	
 	request(omdbMovie, function (error, response, body) {
 		if (error) {
 			return console.log(error);
 		}
-		//printint out desired information
+		// desired information
 		console.log("Title of the movie: " + JSON.parse(body).Title);
 		console.log("Year the movie came out: " + JSON.parse(body).Year);
 		console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
@@ -165,9 +155,7 @@ function movieCall(omdbRequest) {
 		console.log("Movie plot: " + JSON.parse(body).Plot);
 		console.log("Actors in the movie: " + JSON.parse(body).Actors);
 	});
-
 };
-
 
 //function for the do-what-it-says call
 function youCanDoIt() {
@@ -179,7 +167,6 @@ function youCanDoIt() {
 			//the split occurs wherever a comma exists.
 			//each split string is put into an index in an array call "theSplit"
 			var theSplit = data.split(",");
-
 			if (theSplit[0] === "spotify-this-song") {
 				songChoice = theSplit[1];
 
@@ -198,8 +185,6 @@ function youCanDoIt() {
 
 			else if (theSplit[0] === "my-tweets") {
 				myTweets.screen_name = theSplit[1];
-				//console.log(myTweets.screen_name); //test
-
 				//execute twitterCall, pulls tweets from whatever account theSplit[1] is
 				//if testing this call out, DO NOT USE " " OR ' ' AROUND THE SCREEN NAME FOR INDEX 1 
 				//IN random.txt!! 
@@ -209,9 +194,7 @@ function youCanDoIt() {
 			else {
 				console.log("Error: There's a problem with this call.")
 			}
-
-			//console.log(data); //test
-			//console.log(theSplit); //test
+		
 
 		}
 
